7d1e7ae899893564c17171fb7071354f37e0cec2
Timur Shevlyakov <schesoft@gmail.com>
2026-08-14 17:44:27 +0200

    feat: add edge health indicator package

---
+++ src/indicator/__init__.py
@@ -0,0 +1,5 @@
+"""Physical edge health indicator support."""
+
+from .model import EdgeHealthState, IndicatorPattern, pattern_for_state
+
+__all__ = ["EdgeHealthState", "IndicatorPattern", "pattern_for_state"]
